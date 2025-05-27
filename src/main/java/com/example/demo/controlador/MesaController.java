f0aeef957d945b9056cf50e11d5dea06f8c64099
Juan Osorio <osorio.juan.2254@ema.edu.co>
2025-05-27 00:02:13 -0500

    Solcuionando Problema con el mesa controller

--- src/main/java/com/example/demo/controlador/MesaController.java
+++ src/main/java/com/example/demo/controlador/MesaController.java
@@ -26,6 +26,7 @@ public class MesaController {
     public MesaController(MesaService mesaService, JwtService jwtService) {
         this.jwtService = jwtService;
         this.mesaService = mesaService;
+        
     }
 
     @GetMapping
